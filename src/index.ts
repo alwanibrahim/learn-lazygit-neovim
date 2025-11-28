59bfa7180fc16f21bf4186bb76335d9e42d8785b
alwanibrahim <alwanibrahim142@gmail.com>
2025-11-28 12:36:30 +0700

    class

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
-class User {
-  constructor(parameters) {
-    
+class  Animal{
+  santai(){
+  return "sangat santai kawan"
   }
 }
+
+
+
+class Dog extends Animal {
+  override santai(): string {
+    
+  }
+ }
